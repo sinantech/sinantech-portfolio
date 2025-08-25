fee1c0cdaf7d409e29d1f71d34b8ebf8b2126cfe
sinantech <sinancemozer@gmail.com>
2025-08-25 13:59:14 +0300

    v0.8 Main Navigation Adjusted

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const page = () => {
+const about = () => {
   return <div>about</div>;
 };
 
-export default page;
+export default about;
--- components/NavLinks.jsx
+++ components/NavLinks.jsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const links = [
@@ -25,12 +28,28 @@ const links = [
 ];
 
 const NavLinks = ({ containerStyles }) => {
+  const pathName = usePathname();
   return (
     <ul className={containerStyles}>
       {links.map((link, index) => {
+        // determine if the current link matches the active route
+        const isActive = pathName === link.path;
+        // calculate the number of characters in the link name
+        const charLength = link.name.length;
+        // set the line with based on character length
+        const lineWidth = charLength > 5 ? "after:w-[120%]" : "after:w-[90%]";
+        console.log(charLength);
         return (
-          <Link href={link.path} key={index}>
-            {link.name}
+          <Link
+            href={link.path}
+            key={index}
+            className={`relative text-lg uppercase text-white ${
+              isActive &&
+              `after:content-[''] after:block after:absolute after:left-0 after:top-1/2 ${lineWidth} 
+              after:h-[4px] after:bg-accent after:-translate-y-1/2 after:z-0`
+            }`}
+          >
+            <span className="relative z-10">{link.name}</span>
           </Link>
         );
       })}
