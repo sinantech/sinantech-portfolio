d2cc8a21f247e4b58e05038448a907cdb1c65068
sinantech <sinancemozer@gmail.com>
2025-08-25 11:05:16 +0300

    v0.4 Asset and New Components Added

---
+++ components/MainNav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainNav = () => {
+  return <div>main nav</div>;
+};
+
+export default MainNav;
---
+++ components/NavLinks.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavLinks = () => {
+  return <div>nav links</div>;
+};
+
+export default NavLinks;
