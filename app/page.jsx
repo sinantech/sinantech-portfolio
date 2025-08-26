5b09dc927864aee7b6f4d9ffa98318722f8b4058
sinantech <sinancemozer@gmail.com>
2025-08-26 21:29:17 +0300

    v1.7 Socials Icons and Contact Infos Created

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,16 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { TypeAnimation } from "react-type-animation";
+import { MdArrowOutward } from "react-icons/md";
+import { HiOutlinePhone, HiOutlineMail } from "react-icons/hi";
+
 // Components
 import Image from "next/image";
 import Blob from "@/components/Blob";
 import avatarImg from "@/public/assets/avatar.png";
+import Socials from "@/components/Socials";
+
 const Home = () => {
   return (
     <motion.section
@@ -16,7 +22,53 @@ const Home = () => {
       className="h-screen flex items-center"
     >
       <div className="flex flex-col xl:flex-row items-center justify-between w-full">
-        <div className="w-full xl:w-[550px]">text</div>
+        {/* text */}
+        <div className="w-full xl:w-[550px] flex flex-col items-center xl:items-start text-center xl:text-left">
+          <h1 className="h1 flex-1 mb-[28px]">
+            Hi,I'm Jake, <br />
+            <TypeAnimation
+              sequence={["Web Developer", 2000, "Web Designer", 2000]}
+              wrapper="span"
+              speed={40}
+              className="text-accent"
+              repeat={Infinity}
+              cursor={false}
+            />
+          </h1>
+          <p className="max-w-[500px] mb-[44px]">
+            I build visually captivating, user friendly websites and web apps
+            that transform your ideas into seamless, engaging digital
+            experiences.
+          </p>
+          <button className="btn btn-lg btn-accent mb-16">
+            <div className="flex items-center gap-3">
+              <span>Let's Talk</span>
+              <MdArrowOutward className="text-xl" />
+            </div>
+          </button>
+          {/* contact info */}
+          <div className="flex flex-col xl:flex-row xl:items-center gap-4 xl:gap-8">
+            {/* phone */}
+            <div className="flex items-center gap-4 text-xl">
+              <span className="text-accent">
+                <HiOutlinePhone className="tezt-xl" />
+              </span>
+              <span>+40 22 33 44 55</span>
+            </div>
+            {/* mail */}
+            <div className="flex items-center gap-4 text-xl">
+              <span className="text-accent">
+                <HiOutlineMail className="tezt-xl" />
+              </span>
+              <span>jake@gmail.com</span>
+            </div>
+          </div>
+          {/* socials */}
+          <div>
+            <Socials containerStyles="flex 2xl:flex-col gap-6" />
+          </div>
+        </div>
+        {/* blob & image */}
         <div className="hidden xl:block flex-1 relative z-20">
           {/* blob */}
           <Blob containerStyles="w-[560px] h-[560px]" />
@@ -30,7 +82,7 @@ const Home = () => {
             className="absolute -top-16 left-[120px]"
           />
           {/* overlay gradient */}
-          <div className="w-full h-[164px] bg-pink-50/10 absolute bottom-0 left-0 right-0"></div>
+          <div className="w-full h-[164px] absolute bottom-0 left-0 right-0 bg-gradient-to-t from-primary via-primary/90 to-primary/40"></div>
         </div>
       </div>
     </motion.section>
