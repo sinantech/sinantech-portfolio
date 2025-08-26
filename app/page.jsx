fdb1b6a66fd2a127109f6b9bb9741072e6812ce8
sinantech <sinancemozer@gmail.com>
2025-08-26 13:54:58 +0300

    v1.3 Home Page Design and Blob

--- app/page.jsx
+++ app/page.jsx
@@ -10,8 +10,13 @@ const Home = () => {
         opacity: 1,
         transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
       }}
+      className="h-screen flex items-center"
     >
-      home
+      <div className="flex flex-col xl:flex-row items-center justify-between w-full">
+        <div className="w-full xl:w-[550px]">text</div>
+        <div className="hidden xl:block flex-1 relative z-20">image</div>
+        {/* blob */}
+      </div>
     </motion.section>
   );
 };
---
+++ components/Blob.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Blob = () => {
+  return <div></div>;
+};
+
+export default Blob;
