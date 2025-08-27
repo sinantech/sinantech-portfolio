5ceaf5ef86ab26ec32a48456485d030ac4dbc9d8
sinantech <sinancemozer@gmail.com>
2025-08-27 18:25:42 +0300

    v2.5 Scroll Components Created

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import Image from "next/image";
 
 const about = () => {
   return (
@@ -10,8 +12,14 @@ const about = () => {
         opacity: 1,
         transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
       }}
+      className="h-screen flex items-center"
     >
-      about
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row items-center gap-24 w-fit h-[680px]">
+          <div className="flex-1 bg-pink-50/10">1</div>
+          <div className="flex-1 bg-blue-50/10">2</div>
+        </div>
+      </div>
     </motion.section>
   );
 };
