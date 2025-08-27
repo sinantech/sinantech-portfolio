cc1d018e5a6d8500406d368dd69d8e08deeaab33
sinantech <sinancemozer@gmail.com>
2025-08-27 15:05:01 +0300

    v2.4 Homepage and Responsive Design Completed

--- components/MainNav.jsx
+++ components/MainNav.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import NavLinks from "./NavLinks";
+import Logo from "./Logo";
+import { MdFileDownload } from "react-icons/md";
 
 const MainNav = () => {
   return (
-    <nav>
-      <NavLinks containerStyles="flex flex-col gap-6" />
+    <nav className="w-full pt-16">
+      <div className="flex flex-col h-full items-center justify-between">
+        <Logo />
+        <NavLinks containerStyles="flex flex-col gap-6" />
+        <button className="btn btn-lg btn-tertiary mb-16">
+          <div className="flex items-center gap-3">
+            <span>Download CV </span>
+            <MdFileDownload className="text-xl" />
+          </div>
+        </button>
+      </div>
     </nav>
   );
 };
