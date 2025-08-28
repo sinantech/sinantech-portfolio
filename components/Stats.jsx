4038340b6deeba9213b4e3fe3f79246578ce806c
sinantech <sinancemozer@gmail.com>
2025-08-28 11:42:14 +0300

    v2.8 Stats Component Completed

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -2,7 +2,7 @@ import CountUp from "react-countup";
 
 const Stats = () => {
   return (
-    <div>
+    <div className="flex flex-col xl:flex-row xl:items-center gap-8 xl:gap-12">
       <div className="flex gap-4">
         <div className="text-5xl font-bold text-accent flex">
           <CountUp end={12} duration={5} delay={2} className="block w-[60px]" />
@@ -10,7 +10,16 @@ const Stats = () => {
         </div>
         <span>
           Years <br />
-          experience
+          Experience
+        </span>
+      </div>
+      <div className="flex gap-4">
+        <div className="text-5xl font-bold text-accent flex">
+          <CountUp end={43} duration={5} delay={2} className="block w-[60px]" />
+        </div>
+        <span>
+          Clients <br />
+          worldwide
         </span>
       </div>
     </div>
