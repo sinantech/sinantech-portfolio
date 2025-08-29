8293f770c60001e98bdda4f95495693b331dadd1
sinantech <sinancemozer@gmail.com>
2025-08-29 11:59:16 +0300

    v3.3 Info Section Adjustments

--- components/Info.jsx
+++ components/Info.jsx
@@ -6,7 +6,20 @@ import {
 } from "react-icons/hi";
 
 const Info = () => {
-  return <div>info</div>;
+  return (
+    <div className="flex flex-col gap-6 md:gap-10">
+      <div>
+        <div className="w-[280px] flex items-start gap-4">
+          <HiOutlineUser className="text-2xl mt-1 text-accent" />
+          <div>
+            <p className="text-lg">Date of Birth</p>
+            <p>21 June 1996</p>
+          </div>
+        </div>
+      </div>
+      <div>2</div>
+    </div>
+  );
 };
 
 export default Info;
