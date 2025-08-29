113f4ece79e2f2b6bb1774fedec5deb1d689229a
sinantech <sinancemozer@gmail.com>
2025-08-29 13:10:04 +0300

    v3.4 Info Details Completed

--- components/Info.jsx
+++ components/Info.jsx
@@ -8,7 +8,7 @@ import {
 const Info = () => {
   return (
     <div className="flex flex-col gap-6 md:gap-10">
-      <div>
+      <div className="flex flex-col md:flex-row gap-4">
         <div className="w-[280px] flex items-start gap-4">
           <HiOutlineUser className="text-2xl mt-1 text-accent" />
           <div>
@@ -16,8 +16,31 @@ const Info = () => {
             <p>21 June 1996</p>
           </div>
         </div>
+        <div className="w-[280px] flex items-start gap-4">
+          <HiOutlineMail className="text-2xl mt-1 text-accent" />
+          <div>
+            <p className="text-lg">Email Address</p>
+            <p>jake@gmail.com</p>
+          </div>
+        </div>
       </div>
-      <div>2</div>
+
+      <div className="flex flex-col md:flex-row gap-4">
+        <div className="w-[280px] flex items-start gap-4">
+          <HiOutlinePhone className="text-2xl mt-1 text-accent" />
+          <div>
+            <p className="text-lg">Phone</p>
+            <p>+1234567890</p>
+          </div>
+        </div>
+        <div className="w-[280px] flex items-start gap-4">
+          <HiOutlineLocationMarker className="text-2xl mt-1 text-accent" />
+          <div>
+            <p className="text-lg">Location</p>
+            <p>Berlin,Germany</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
