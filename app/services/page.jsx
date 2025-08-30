343fefa89c9bc6995cd643ecab4b80b0132cf53f
sinantech <sinancemozer@gmail.com>
2025-08-30 15:44:16 +0300

    v3.8 Service Page Design Deployed

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,6 +1,44 @@
 "use client";
 
 import { motion } from "framer-motion";
+// import swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+// import swiper modules
+import { Pagination } from "swiper/modules";
+// import swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import Image from "next/image";
+import { MdOutlineArrowOutward, MdArrowRightAlt } from "react-icons/md";
+
+const service = [
+  {
+    icon: "/assets/services/design.svg",
+    href: "",
+    title: "Website interface Design",
+  },
+  {
+    icon: "/assets/services/frontend.svg",
+    href: "",
+    title: "Frontend Development",
+  },
+  {
+    icon: "/assets/services/backend.svg",
+    href: "",
+    title: "Backend Development",
+  },
+  {
+    icon: "/assets/services/seo.svg",
+    href: "",
+    title: "Search Engine Optimization",
+  },
+  {
+    icon: "/assets/services/video.svg",
+    href: "",
+    title: "Video Production",
+  },
+];
 
 const services = () => {
   return (
