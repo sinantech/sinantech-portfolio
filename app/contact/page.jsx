9e37c27a52e1d70db69bb9d9726d8259bb7b3e8d
sinantech <sinancemozer@gmail.com>
2025-09-04 13:34:06 +0300

    v5.2 Contact Page Adjustments

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -5,6 +5,17 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
+
+import { HiOutlineMapPin, HiOutlineArrowLongRight } from "react-icons/hi2";
+import { HiOutlinePhone, HiOutlineMail } from "react-icons/hi";
+
 const contact = () => {
   return (
     <motion.section
@@ -13,8 +24,21 @@ const contact = () => {
         opacity: 1,
         transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
       }}
+      className="h-screen flex items-center py-24 xl:py-0"
     >
-      contact
+      <div
+        className="container mx-auto w-full h-full flex flex-col items-center xl:justify-center xl:overflow-hidden scrollbar 
+      scrollbar-thumb-accent scrollbar-track-accent/5 overflow-y-scroll xl:overflow-y-visible"
+      >
+        <div className="w-full">
+          <div className="flex flex-col xl:flex-row gap-6">
+            {/* info text */}
+            <div>info text</div>
+            {/* form */}
+            <div>form</div>
+          </div>
+        </div>
+      </div>
     </motion.section>
   );
 };
