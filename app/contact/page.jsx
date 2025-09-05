712bfba1e39424a867cdb4b80709f62e9fe3b69f
sinantech <sinancemozer@gmail.com>
2025-09-05 13:59:54 +0300

    v5.9 Textarea Completed

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -24,8 +24,94 @@ const contact = () => {
         opacity: 1,
         transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
       }}
+      className="h-screen flex items-center py-24 xl:py-0"
     >
-      contact
+      <div
+        className="container mx-auto w-full h-full flex flex-col items-center xl:justify-center
+      xl:overflow-hidden scrollbar scrollbar-thumb-accent scrollbar-track-accent/5 
+      overflow-y-scroll xl:overflow-y-visible"
+      >
+        <div className="w-full">
+          <div className="flex flex-col xl:flex-row gap-6">
+            {/* info text */}
+            <div>info text</div>
+            {/* form */}
+            <div className="flex-1">
+              <form className="flex flex-col gap-6 items-start">
+                {/* first and last name */}
+                <div className="flex flex-col xl:flex-row gap-6 w-full">
+                  <div className="w-full">
+                    <Label htmlFor="name">
+                      Firstname <span className="text-accent">*</span>
+                    </Label>
+                    <Input
+                      id="firstname"
+                      name="firstname"
+                      placeholder="First Name"
+                      required
+                    />
+                  </div>
+                  <div className="w-full">
+                    <Label htmlFor="name">
+                      Lastname <span className="text-accent">*</span>
+                    </Label>
+                    <Input
+                      id="lastname"
+                      name="lastname"
+                      placeholder="Last Name"
+                      required
+                    />
+                  </div>
+                </div>
+                {/* email */}
+                <div className="w-full">
+                  <Label htmlFor="name">
+                    Email <span className="text-accent">*</span>
+                  </Label>
+                  <Input
+                    id="email"
+                    name="email"
+                    placeholder="youremail@email.com"
+                    required
+                  />
+                </div>
+                {/* select */}
+                <div className="w-full">
+                  <Label htmlFor="name">
+                    I'm interested in <span className="text-accent">*</span>
+                  </Label>
+                  <Select name="service" required>
+                    <SelectTrigger
+                      id="service"
+                      className="w-full !h-12 bg-white/5 border-white/10 px-4"
+                    >
+                      <SelectValue placeholder="Choose Here" />
+                    </SelectTrigger>
+                    <SelectContent className="bg-black border-white/20">
+                      <SelectItem value="webdev">Web Development</SelectItem>
+                      <SelectItem value="uiux">UI&UX Design</SelectItem>
+                      <SelectItem value="logo">Logo Design</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+                {/* textarea */}
+                <div className="w-full">
+                  <Label htmlFor="name">
+                    Message <span className="text-accent">*</span>
+                  </Label>
+                  <Textarea
+                    id="message"
+                    name="message"
+                    placeholder="Write Your Message..."
+                    className="min-h-[160px] bg-white/5 border-white/10
+                  focus-visible:border-accent focus-visible:ring-accent focus-visible:ring-[1px] resize-none p-4 selection:bg-accent placeholder:text-wihte/50"
+                  ></Textarea>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </motion.section>
   );
 };
